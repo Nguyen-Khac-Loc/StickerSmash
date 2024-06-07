0ded92ec9526b4115673bfb869e079e1feb9645b
LocNguyen <locn1504@gmail.com>
2024-06-07 17:11:07 +0700

    in progress

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar, StyleSheet, View } from 'react-native';
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+import images from './constants/images';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.imageContainer}>
+				<ImageViewer placeholderImageSource={images.placehoder} />
+			</View>
+			<View style={styles.footerContainer}>
+				<Button theme="primary" label="Choose a photo" />
+				<Button label="Use this photo" />
+			</View>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#25292e',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	imageContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+
+	footerContainer: {
+		flex: 1 / 3,
+		alignItems: 'center',
+	},
 });
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import placehoder from '../assets/images/background-image.png';
+
+export default {
+	placehoder
+};
