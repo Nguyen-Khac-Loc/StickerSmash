32095988adc8395cd6c1a4167e2e173eeaddc7cf
LocNguyen <locn1504@gmail.com>
2024-06-08 09:23:25 +0700

    feat: image picker with expo-image-picker

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar, StyleSheet, View } from 'react-native';
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
 import images from './constants/images';
 
+import * as ImagePicker from 'expo-image-picker';
 export default function App() {
+	const [selectedImage, setSelectedImage] = useState(null);
+	const pickImageAsync = async () => {
+		let result = await ImagePicker.launchImageLibraryAsync({
+			allowsEditing: true,
+			quality: 1,
+		});
+
+		if (!result.canceled) {
+			setSelectedImage(result.assets[0].uri);
+		} else {
+			alert('You did not select any image.');
+		}
+	};
 	return (
 		<View style={styles.container}>
 			<View style={styles.imageContainer}>
-				<ImageViewer placeholderImageSource={images.placehoder} />
+				<ImageViewer placeholderImageSource={images.placehoder}
+					selectedImage={selectedImage} />
 			</View>
 			<View style={styles.footerContainer}>
-				<Button theme="primary" label="Choose a photo" />
 				<Button label="Use this photo" />
+				<Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
 			</View>
 			<StatusBar style="auto" />
 		</View>
